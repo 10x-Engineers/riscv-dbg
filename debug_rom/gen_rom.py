7c4155f24fa9d649c10d34a1c8b1211fa42063fa
tufail rizvi <tufail.rizvi@10xengineers.ai>
2026-07-24 17:29:31 +0500

    Resolved the address migration issue

--- debug_rom/gen_rom.py
+++ debug_rom/gen_rom.py
@@ -43,7 +43,6 @@ license = """\
 module = """\
 module $filename (
   input  logic         clk_i,
-  input  logic         rst_ni,
   input  logic         req_i,
   input  logic [63:0]  addr_i,
   output logic [63:0]  rdata_o
@@ -56,15 +55,12 @@ module $filename (
 $content
   };
 
-  logic [$$clog2(RomSize)-1:0] addr_d, addr_q;
+  // rst_ni removed: dm_mem.sv's v1.0 update no longer wires a reset to this module
+  logic [$$clog2(RomSize)-1:0] addr_q;
 
-  assign addr_d = req_i ? addr_i[$$clog2(RomSize)-1+3:3] : addr_q;
-
-  always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) begin
-      addr_q <= '0;
-    end else begin
-      addr_q <= addr_d;
+  always_ff @(posedge clk_i) begin
+    if (req_i) begin
+      addr_q <= addr_i[$$clog2(RomSize)-1+3:3];
     end
   end
 
